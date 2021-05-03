39859b415af6e3a03cd60da2968f271553d5dc18
OSumapriya <sumapriya9912@gmail.com>
2021-05-03 16:07:08 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -34,10 +34,11 @@ namespace API
             }
             );
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            });       
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,12 +55,14 @@ namespace API
 
             app.UseRouting();
 
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
-            });
+                endpoints.MapControllers();                                                                                                                                 
+            });                                          
         }
     }
 }
